63099e446977b99c2c44f4d3081fa476c4aeb788
Achal Agasthya <47064104+achgasthya07@users.noreply.github.com>
2021-12-14 20:21:05 +0200

    Update constraints_dq.m

--- constraints_dq.m
+++ constraints_dq.m
@@ -16,6 +16,16 @@ for rj = mbs.joints.revolute
     c_idx = c_idx + 2;
 end
 
+for rj = mbs.joints.translational
+    q1_idx = body_idx(rj.body1);
+    q2_idx = body_idx(rj.body2);
+    q1 = q(q1_idx);
+    q2 = q(q2_idx);
+    Cq(c_idx + (1:2), q1_idx) = [eye(2), Omega * A1 * rj.s1];
+    Cq(c_idx + (1:2), q2_idx) = -[eye(2), Omega * A2 * rj.s2];
+    c_idx = c_idx + 2;
+end
+
 for sj = mbs.joints.simple
     q_idx = body_idx(sj.body);
     Cq(c_idx + 1, q_idx(sj.coord)) = 1;
